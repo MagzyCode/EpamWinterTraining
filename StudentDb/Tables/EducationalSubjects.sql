d75b0e2fdac3b790c5a77928bbee6d5415c5d081
Kazarevich Mikhail <mihailkazarevich@mail.ru>
2021-02-21 12:39:48 +0300

    re-added scripts

---
+++ StudentDb/Tables/EducationalSubjects.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[EducationalSubjects] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [SubjectName] NVARCHAR (MAX) NOT NULL,
+    [SubjectType] NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
